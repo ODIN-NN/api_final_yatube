200ab2d8c195a2e57c3eeaf7be44752cfc5d7c7a
Alexander_Odintsov <odintsov.alexander.nn@yandex.com>
2022-09-29 15:55:17 +0300

    hw_09_try01

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
 # api_final
-api final
+
+Учебный проект Api_final предстовляет собой набор функций 
+для получения, добавления и редактирования информации но портале Yatube.
+# Как запустить проект:
+Клонировать репозиторий:
+
+```
+git clone https://github.com/ODIN-NN/api_final_yatube.git
+```
+
+Перейти в директорию с помощью командной строки:
+
+```
+cd api_final_yatube
+```
+
+Cоздать и активировать виртуальное окружение:
+
+```
+python -m venv env
+```
+
+```
+source venv/Scripts/activate
+```
+
+Установить зависимости из файла requirements.txt:
+
+```
+python -m pip install --upgrade pip
+```
+
+```
+pip install -r requirements.txt
+```
+
+Выполнить миграции:
+
+```
+python manage.py migrate
+```
+
+Запустить проект:
+
+```
+python manage.py runserver
+```
+ # Примеры запросов и ответов
+ POST запрос к эндпоинту http://127.0.0.1:8000/api/v1/posts/    
+ с данными 
+{
+    "text": "Just text"
+}
+создаст пост с заданными данными   
+ ____
+ GET запрос к эндпоинту http://127.0.0.1:8000/api/v1/posts/    
+ вернёт список всех постов. Ответ будет выглядеть следующим образом:    
+ ```python
+ [
+    {
+        "id": 1,
+        "author": "alex",
+        "text": "Just text",
+        "pub_date": "2022-09-29T12:37:41.789918Z",
+        "image": null,
+        "group": null
+    }
+]
+```
--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
 
 
-from posts.models import Comment, Post
+from posts.models import Comment, Post, Follow, Group, User
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -21,3 +22,39 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Comment
+        read_only_fields = ('post', )
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = '__all__'
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        queryset=User.objects.all(),
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
+    )
+    following = serializers.SlugRelatedField(
+        queryset=User.objects.all(),
+        slug_field='username',
+    )
+
+    class Meta:
+        model = Follow
+        fields = '__all__'
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following')
+            )
+        ]
+
+    def validate_following(self, value):
+        if value == self.context['request'].user:
+            raise serializers.ValidationError(
+                ['Подписаться на самого себя невозможно.'])
+        return value
